822ff619cdf26c211940e4dd1e1353f1a2575ef0
SamaWyna <etornamasamoah@gmail.com>
2025-10-25 22:08:15 +0100

    Fix Docker backend package path

---
+++ sankofa_backend/core/asgi.py
@@ -0,0 +1,21 @@
+"""ASGI config for core project."""
+from __future__ import annotations
+
+import os
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.production")
+
+django_asgi_app = get_asgi_application()
+
+websocket_urlpatterns: list = []
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+    }
+)
