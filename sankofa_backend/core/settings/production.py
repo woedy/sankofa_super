1ad933d421a38936af314ed406add19e258e84c8
Sama <etornamasamoah@gmail.com>
2025-10-30 19:18:09 +0000

    Django-admin-css

--- sankofa_backend/core/settings/production.py
+++ sankofa_backend/core/settings/production.py
@@ -6,3 +6,13 @@ DEBUG = False
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
+# WhiteNoise configuration for efficient static file serving
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
